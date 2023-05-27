b665d5c5818bd53fea82a11cc41f7b7461aa5bd7
carmine <carmine.coppola001@studenti.uniparthenope.it>
2023-05-27 13:49:15 +0200

    Somma

--- Thread/es4.c
+++ Thread/es4.c
@@ -4,78 +4,76 @@
    parziali per ottenere la somma totale dell'intero array.
 */
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 
 #define size 10
 
-int somma_parziale = 0;
-int somma_parziale2 = 0;
+int somma_parziale = 0; // Variabile per la somma parziale del primo thread
+int somma_parziale2 = 0; // Variabile per la somma parziale del secondo thread
 
-void *routine(int array[size])
+// Funzione eseguita dal primo thread
+void *routine(void *arg)
 {
-    int somma_parziale = 0;
-    
-    for (int i = 0; i < size/2; i++)
+    int *array = (int *)arg; // Casting dell'argomento a un puntatore a un array di interi
+    for (int i = 0; i < size / 2; i++)
     {
-        somma_parziale += array[i];
+        somma_parziale += array[i]; // Calcolo della somma parziale della prima metà dell'array
     }
-    printf("\nLa prima somma parziale è: %d ",somma_parziale);
-
-    return somma_parziale;
+    printf("\nLa prima somma parziale è: %d ", somma_parziale);
+    pthread_exit(NULL); // Terminazione del thread
 }
 
-void *routine2(int array[size])
+// Funzione eseguita dal secondo thread
+void *routine2(void *arg)
 {
-    
-    for (int i = (size/2) + 1; i < size; i++)
+    int *array = (int *)arg; // Casting dell'argomento a un puntatore a un array di interi
+    for (int i = size / 2; i < size; i++)
     {
-        somma_parziale2 += array[i];
+        somma_parziale2 += array[i]; // Calcolo della somma parziale della seconda metà dell'array
     }
-    printf("\nLa seconda somma parziale è: %d ",somma_parziale2);
-
-    return somma_parziale2;
+    printf("\nLa seconda somma parziale è: %d ", somma_parziale2);
+    pthread_exit(NULL); // Terminazione del thread
 }
 
 int main()
 {
-    int array[size] = {25,30,87,11,10,2,4,23,9,7};
+    int array[size] = {25, 30, 87, 11, 10, 2, 4, 23, 9, 7};
 
-    pthread_t *th1;
-    pthread_t *th2;
-    int somma_finale = 0;
+    pthread_t th1; // Identificatore del primo thread
+    pthread_t th2; // Identificatore del secondo thread
+    int somma_finale = 0; // Variabile per la somma finale dell'intero array
 
-    //Creazione dei Thread
-    if (pthread_create(&th1, NULL, &routine, NULL) != 0)
-    {
-        printf("\nErrore nella creazione del thread");
-        return -1;
-    }
-    
-    if (pthread_create(&th2, NULL, &routine2, NULL) != 0)
+    // Creazione dei Thread
+    if (pthread_create(&th1, NULL, routine, (void *)array) != 0)
     {
         printf("\nErrore nella creazione del thread");
         return -1;
     }
 
+    if (pthread_create(&th2, NULL, routine2, (void *)array) != 0)
+    {
+        printf("\nErrore nella creazione del thread");
+        return -1;
+    }
 
-    //Fase di JOIN
-    if(pthread_join(&th1,NULL) != 0)
+    // Fase di JOIN
+    if (pthread_join(th1, NULL) != 0)
     {
         printf("\nErrore nella fase di join");
         return -2;
     }
-    
-    if(pthread_join(&th2,NULL) != 0)
+
+    if (pthread_join(th2, NULL) != 0)
     {
         printf("\nErrore nella fase di join");
         return -2;
     }
 
-    somma_finale = somma_parziale + somma_parziale2;
+    somma_finale = somma_parziale + somma_parziale2; // Calcolo della somma finale dell'intero array
 
-    printf("\nLa somma finale è: %d",somma_finale);
+    printf("\nLa somma finale è: %d \n", somma_finale);
 
     return 0;
 }
