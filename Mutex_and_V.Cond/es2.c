b05698a3be48126ea3eb95ad011d2953212b281f
carmine <carmine.coppola001@studenti.uniparthenope.it>
2023-06-02 01:22:42 +0200

    da finire

--- Mutex_and_V.Cond/es2.c
+++ Mutex_and_V.Cond/es2.c
@@ -9,16 +9,19 @@
                dispari della riga i-esima;
         • Calcolate le somme parziali, si provvederà a ricercarne il minimo ed a stamparlo a video;
 */
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <time.h>
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+#include<time.h>
 
 #define SIZE 4
 
 int** matrix;
 int somma_totale[SIZE];
 
+
+//Crea e alloca dinamicamente la matrice casuale da 0 255
 int** generaMatrice()
 {
     int** matrix = (int**)malloc(SIZE * sizeof(int*));
@@ -28,14 +31,16 @@ int** generaMatrice()
         matrix[i] = (int*)malloc(SIZE * sizeof(int));
         for (int j = 0; j < SIZE; j++)
         {
-            matrix[i][j] = rand() % 256; // Genera un numero casuale tra 0 e 255
-        }
+            matrix[i][j] = rand()% 255 + 1; // Genera un numero casuale tra 0 e 255
+        } 
     }
     return matrix;
 }
 
+//Stampa la matrice
 void stampaMatrice(int** matrix)
 {
+
     for (int i = 0; i < SIZE; i++)
     {
         for (int j = 0; j < SIZE; j++)
@@ -46,25 +51,28 @@ void stampaMatrice(int** matrix)
     }
 }
 
-void* routine(void* arg)
+void* routine(void *arg)
 {
     int indice = *(int*)arg;
 
     int somma = 0;
-
-    if (indice % 2 == 0)
+    
+    //Se ha l'indice pari
+    if(indice %2 == 0)
     {
-        for (int i = 0; i < SIZE; i += 2)
+        for (int i = 0; i < SIZE; i+=2)
         {
             somma += matrix[indice][i];
         }
     }
+    //Se ha l'indice dispari
     else
     {
-        for (int i = 1; i < SIZE; i += 2)
+        for (int i = 1; i < SIZE; i+=2)
         {
             somma += matrix[indice][i];
         }
+        
     }
 
     somma_totale[indice] = somma;
@@ -83,7 +91,7 @@ int minimo(int* array)
     return min;
 }
 
-int main(int argc, char* args[])
+int main(int argc, char *args[])
 {
     srand(time(NULL));
 
@@ -94,6 +102,7 @@ int main(int argc, char* args[])
     pthread_t th[SIZE];
     int indici[SIZE];
 
+    //Creo thread
     for (int i = 0; i < SIZE; i++)
     {
         indici[i] = i;
@@ -102,25 +111,32 @@ int main(int argc, char* args[])
             printf("\nErrore nella creazione dei thread\n");
             return -1;
         }
+        
     }
 
+    int result[SIZE];
+
+    //Faccio JOIN
     for (int i = 0; i < SIZE; i++)
     {
-        if (pthread_join(th[i], NULL) != 0)
+        if (pthread_join(th[i], (void**)&result[i]) != 0)
         {
             printf("\nErrore nella fase di JOIN dei thread\n");
             return -2;
         }
+        
     }
 
     int min = minimo(somma_totale);
     printf("\nMinimo: %d\n", min);
 
-    for (int i = 0; i < SIZE; i++)
-    {
+    //Libero lo spazio della matrice
+    for (int i = 0; i < SIZE; i++) {
         free(matrix[i]);
     }
     free(matrix);
+
+
 
     return 0;
 }
