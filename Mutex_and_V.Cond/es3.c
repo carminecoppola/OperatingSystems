7e6bc3d18b7b50e7ba4f7c815a50274d47fdfea4
carmine <carmine.coppola001@studenti.uniparthenope.it>
2023-06-03 00:47:12 +0200

    commit

--- Mutex_and_V.Cond/es3.c
+++ Mutex_and_V.Cond/es3.c
@@ -10,3 +10,71 @@
           due processi figli F1 e F2 e terminerà stampando un messaggio di saluto. 
 */
 
+#include<stdlib.h>
+#include<stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+void somma()
+{
+  pid_t pidPadre = getpid();
+  pid_t pidFiglio = getpid();
+
+  int somma = pidPadre + pidFiglio;
+
+  printf("Figlio 1 (PID: %d): Somma = %d\n", pidFiglio, somma);
+  exit(0);
+
+}
+
+void differenza()
+{
+  pid_t pidPadre = getpid();
+  pid_t pidFiglio = getpid();
+
+  int differenza = pidPadre - pidFiglio;
+
+  printf("Figlio 2 (PID: %d): Differenza = %d\n", pidFiglio, differenza);
+  exit(0);
+
+}
+
+int main(int argc, char*argv[])
+{ 
+  
+  int N = atoi(argv[1]);
+
+  if (N < 1 || N > 1000) {
+      printf("Il parametro N deve essere compreso tra 1 e 1000.\n");
+      return 1;
+  }
+  
+  
+  pid_t pid_figlio1, pid_figlio2;
+
+  pid_figlio1 = fork();
+
+  if (N % 2 == 0)
+  {
+    printf("\nIl pid è pari");
+    signal(SIGUSR1, somma);
+  }
+  else
+  {
+    pid_figlio2 = fork();
+    if (pid_figlio2 == 0)
+    {
+      signal(SIGUSR2, differenza);
+    }
+    else
+    {
+      printf("\nIl pid è dispari");
+      kill(pid_figlio2,SIGUSR2);
+    }
+
+  }
+
+  return 0;
+}
