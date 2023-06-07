5ede595be7c669c41215a83e38087be368895b19
carmine <carmine.coppola001@studenti.uniparthenope.it>
2023-06-08 00:22:04 +0200

    Non completo

--- es1.c
+++ es1.c
@@ -4,64 +4,100 @@
     Implementare il programma con i processi ed i semafori Posix.   */
 
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <time.h>
 
 int n = 0;
+sem_t semPadre;
+sem_t semFiglio;
 
-
-void *routinePadre( void* arg)
+void *routinePadre(void *arg)
 {
+    FILE *file = (FILE *)arg;
 
+    sem_wait(&semPadre);
 
+    int randomNumber = rand() % n + 1;
 
+    printf("\nHo generato il numero [%d]", randomNumber);
+
+    fprintf(file, "%d\n", randomNumber);
+
+    sem_post(&semFiglio);
+
+    pthread_exit(NULL);
 }
 
-void *routineFiglio( void* arg)
+void *routineFiglio(void *arg)
 {
+    FILE *file = (FILE *)arg;
 
+    sem_wait(&semFiglio);
 
+    int readNumero;
 
+    rewind(file);
+
+    fscanf(file, "%d", &readNumero);
+
+    printf("\nIl numero letto è %d\n", readNumero);
+
+    sem_post(&semPadre);
+
+    pthread_exit(NULL);
 }
 
-
-
-
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    
+    srand(time(NULL));
+
     n = atoi(argv[1]);
+
+    sem_init(&semPadre, 0, 1);
+    sem_init(&semFiglio, 0, 0);
+
+    FILE *file = fopen("file.txt", "r+");
+
+    if (file == NULL)
+    {
+        printf("\nImpossibile aprire il file.\n");
+        return 1;
+    }
 
     pthread_t thDad, thSon;
 
-    if (pthread_create( &thDad, NULL, routinePadre, NULL) != 0)
+    if (pthread_create(&thDad, NULL, routinePadre, (void *)file) != 0)
     {
         printf("\nErrore nella creazione del thread padre");
         return -1;
     }
 
-    if (pthread_create( &thSon, NULL, routineFiglio, NULL) != 0)
+    if (pthread_create(&thSon, NULL, routineFiglio, (void *)file) != 0)
     {
         printf("\nErrore nella creazione del thread figlio");
         return -1;
     }
 
-
-    if (pthread_join( thDad, NULL) != 0)
+    if (pthread_join(thDad, NULL) != 0)
     {
         printf("\nErrore nella fase di JOIN del thread padre");
         return -2;
     }
 
-    if (pthread_join( thSon, NULL) != 0)
+    if (pthread_join(thSon, NULL) != 0)
     {
         printf("\nErrore nella fase di JOIN del thread figlio");
         return -2;
     }
 
-    
+    fclose(file);
 
+    sem_destroy(&semPadre);
+    sem_destroy(&semFiglio);
 
     return 0;
 }
+
